6e1baa6ff27939f66369553f467478a1a1ebd8c3
Brian Michel <brian.michel@gmail.com>
2009-11-19 12:21:13 -0500

    Added friendships relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :runs
   has_many :courses, :through => :runs
   
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  
   def full_name
     "#{firstName} #{lastName}"   
   end
