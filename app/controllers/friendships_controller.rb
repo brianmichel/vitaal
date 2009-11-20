e3de1619aaf35f9896136cd3719f0c9ab67174d0
Brian Michel <brian.michel@gmail.com>
2009-11-19 20:00:09 -0500

    Added Create and Destroy actions for controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,17 @@
 class FriendshipsController < ApplicationController
   def create
-    
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Made Friendship!"
+      redirect_to root_url
+    else
+      render :action => 'new'
   end
   
   def destroy
-    
+    @friendship = current_user.Friendship.find(params[:id])
+    @friendship.destroy
+    flash[:notice] = "Destroyed Friendship!"
+    redirect_to root_url
   end
 end
