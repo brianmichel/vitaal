2789ca4dcc8ea2671d092949984af41a6a8252b8
Brian Michel <brian.michel@gmail.com>
2009-11-20 22:26:13 -0500

    Added before_filter to remove unauthorized friend requests.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,6 @@
 class FriendshipsController < ApplicationController
+  before_filter :authorize
+  
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
@@ -6,10 +8,11 @@ class FriendshipsController < ApplicationController
       redirect_to root_url
     else
       render :action => 'new'
+    end
   end
   
   def destroy
-    @friendship = current_user.Friendship.find(params[:id])
+    @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
     flash[:notice] = "Destroyed Friendship!"
     redirect_to root_url
