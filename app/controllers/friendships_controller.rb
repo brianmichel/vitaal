721986bc2b0dc1f5b0e754dc2340685b3d108625
Brian Michel <brian.michel@gmail.com>
2009-12-01 11:16:55 -0500

    Added action for creation of new event on new friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,12 @@ class FriendshipsController < ApplicationController
   
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    @event = Events.new(:data => {
+      "friend_name" => User.find(:first, :conditions => {:id => @friendship.friend_id}).name,
+      "created_by" => current_user.name
+    }, :user_id => current_user.id, :event_type => "friend_add")
     if @friendship.save
+      @event.save
       flash[:notice] = "Made Friendship!"
       redirect_to root_url
     else
